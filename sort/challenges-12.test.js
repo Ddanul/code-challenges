c10d0563bc2439aaef9a6496e17901d0855c016f
Danul De Leon <danul.deleon@gmail.com>
2018-09-18 17:34:46 -0700

    solved q4

--- sort/challenges-12.test.js
+++ sort/challenges-12.test.js
@@ -62,7 +62,15 @@ const sortByLength = (strings) => {
 // ------------------------------------------------------------------------------------------------
 
 const alphabetizeBetter = (strs) => {
-  // Solution code here...
+  return strs.sort((a,b) => {
+    if(a.toLowerCase()>b.toLowerCase()){
+      return 1;
+    } else if (a.toLowerCase()<b.toLowerCase()){
+      return -1;
+    } else {
+      return 0;
+    }
+  });
 };
 
 // ------------------------------------------------------------------------------------------------
